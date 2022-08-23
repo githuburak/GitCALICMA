5c377a8b9c46aefb2c0e0172263687b9aaea96b3
bcy <burakcemalyilmaz@gmail.com>
2022-08-24 01:39:55 +0300

    last2

--- src/GitCALICMA/GitHUBKulanimi1.java
+++ src/GitCALICMA/GitHUBKulanimi1.java
@@ -3,5 +3,10 @@ package GitCALICMA;
 public class GitHUBKulanimi1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+<<<<<<< HEAD
+=======
+
+        System.out.println("last" );
+>>>>>>> 2dcca4b (last2)
     }
 }
